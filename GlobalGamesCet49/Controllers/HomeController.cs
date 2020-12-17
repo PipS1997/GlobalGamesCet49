1d67a93ca10a7d9b8b52023662021b487915cf60
PipS <PipS@DESKTOP-FRMKOBB>
2020-12-17 12:24:59 +0000

    Add project files.

---
+++ GlobalGamesCet49/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using GlobalGamesCet49.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GlobalGamesCet49.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sobre()
+        {
+
+            return View();
+        }
+
+        public IActionResult Servicos()
+        {
+
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
